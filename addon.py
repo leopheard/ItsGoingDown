8b3db5b7bef196cee0f5cea363a27bf5df119948
leopheard <leopheard.misc@gmail.com>
2019-10-07 22:47:47 -0400

    upload

--- addon.py
+++ addon.py
@@ -3,7 +3,6 @@ from resources.lib import mainaddon
 plugin = Plugin()
 url1 = "https://itsgoingdown.org/feed/podcast"
 url2 = "https://itsgoingdown.org/podcast/feed/"
-url3 = "https://itsgoingdown.org/category/podcast/feed/"
 @plugin.route('/')
 def main_menu():
     items = [
@@ -14,10 +13,6 @@ def main_menu():
         {
             'label': plugin.get_string(30002),
             'path': plugin.url_for('episodes2'),
-            'thumbnail': "https://itsgoingdown.org/wp-content/uploads/powerpress/igd_square.jpg"},
-        {
-            'label': plugin.get_string(30003),
-            'path': plugin.url_for('episodes3'),
             'thumbnail': "https://itsgoingdown.org/wp-content/uploads/powerpress/igd_square.jpg"},
     ]
     return items
@@ -32,12 +27,6 @@ def episodes2():
     soup2 = mainaddon.get_soup2(url2)
     playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
     items = mainaddon.compile_playable_podcast2(playable_podcast2)
-    return items
-@plugin.route('/episodes3/')
-def episodes3():
-    soup3 = mainaddon.get_soup3(url3)
-    playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
-    items = mainaddon.compile_playable_podcast3(playable_podcast3)
     return items
 if __name__ == '__main__':
     plugin.run()
--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -12,11 +12,6 @@ def get_soup2(url2):
     soup2 = BeautifulSoup(page.text, 'html.parser')
     print("type: ", type(soup2))
     return soup2
-def get_soup3(url3):
-    page = requests.get(url3)
-    soup3 = BeautifulSoup(page.text, 'html.parser')
-    print("type: ", type(soup3))
-    return soup3
 
 def get_playable_podcast1(soup1):
     subjects = []
@@ -68,35 +63,6 @@ def get_playable_podcast2(soup2):
 def compile_playable_podcast2(playable_podcast2):
     items = []
     for podcast in playable_podcast2:
-        items.append({
-            'label': podcast['title'],
-            'thumbnail': podcast['thumbnail'],
-            'path': podcast['url'],
-            'is_playable': True,
-    })
-    return items
-
-def get_playable_podcast3(soup3):
-    subjects = []
-    for content in soup3.find_all('item'):
-        try:
-            link = content.find('enclosure')
-            link = link.get('url')
-            print("\n\nLink: ", link)
-            title = content.find('title')
-            title = title.get_text()
-        except AttributeError:
-            continue
-        item = {
-                'url': link,
-                'title': title,
-                'thumbnail': "https://itsgoingdown.org/wp-content/uploads/powerpress/igd_square.jpg",
-        }
-        subjects.append(item)
-    return subjects
-def compile_playable_podcast3(playable_podcast3):
-    items = []
-    for podcast in playable_podcast3:
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
